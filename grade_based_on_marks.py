fe0556db4bb8f5516a7ed2de08ba552e251b67aa
ItsRakesh97 <Bisht8985@gmail.com>
2026-03-23 06:34:31 +0100

    Create grade_based_on_marks.py

---
+++ grade_based_on_marks.py
@@ -0,0 +1,10 @@
+marks = float,int(input("enter the marks : "))
+
+if marks >= 90:
+    print("grade A")
+elif marks >= 50:
+    print("grade B")
+elif marks >= 33:
+    print("grade C")
+else:
+    print("fail")
