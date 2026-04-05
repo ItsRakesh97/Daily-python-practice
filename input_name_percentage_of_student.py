6cc409c1db23abd4430f3e9f72215285b47b711d
ItsRakesh97 <Bisht8985@gmail.com>
2026-04-05 21:11:47 +0200

    Create input_name_percentage_of_student.py

---
+++ input_name_percentage_of_student.py
@@ -0,0 +1,10 @@
+name = input("enter the name of student: ")
+marks1 = float(input("enter marks of subject 1: "))
+marks2 = float(input("enter marks of subject 2: "))
+marks3 = float(input("enter marks of subject 3: "))
+
+total = marks1 + marks2 + marks3
+percentage = total / 3
+
+print("the name of student is " ,name)
+print("the percentage of the 3 subjects is",percentage)
